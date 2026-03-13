68ed24a8fa651bffe6161c6e6f3e9395fb8c4f5b
Desire <desirecrevan@gmail.com>
2026-03-13 17:18:52 +0300

    hello

---
+++ ehandling.py
@@ -0,0 +1,9 @@
+#print(10/0)
+try:   
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number:"))
+    result = num1/num2
+except ZeroDivisionError:
+    print("You cant divide by zero")
+else:
+    print(f"The answer is {result}")
